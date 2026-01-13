3ac6b66a5d58ae221a599ceb526dfab9b82d4e6b
yspark <youngsue5153@gmail.com>
2026-01-13 11:55:04 +0900

    feat: Step2 Divide classes and Encapsulation

---
+++ src/com/sparta/Calculator.java
@@ -0,0 +1,232 @@
+package com.sparta;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Calculator {
+
+    private List<String> history = new ArrayList<>();
+
+    /**
+     * 데이터의 불변성을 지키기 위한 캡슐화를 잘하는 것이 중요점인듯하다. 그런부분을 바로바로 생각하는게 잘안되는 것 같다.
+     * history 를 그대로 전달하면 외부에서 받아 .add 혹은 .clear를 통해 히스토리 내역을 write 할 수 있다. (불변성 해침) !!
+     * 1. 수정불가 원본: Collections.unmodifiableList(history)
+     * 2. 복사본 전달: new ArrayList<>(history)
+     * 3. 수정불가 복사본: List.copyOf(history)  = Collections.unmodifiableList(new ArrayList<>(history))
+     */
+
+
+//    public List<String> getHistory() {
+//        return history;
+//    }
+
+//    public List<String> getHistory() {
+//        return Collections.unmodifiableList(history);
+//    }
+
+//    public List<String> getHistory() {
+//        return new ArrayList<String>(history);
+//    }
+
+//    public List<String> getHistory() {
+//        return List.copyOf(history);
+//    }
+
+    public List<String> getHistory() {
+        return Collections.unmodifiableList(new ArrayList<>(history));
+    }
+
+
+    public void start() {
+        boolean isRunning = true;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("===나는 계산기야===");
+
+        // menu
+        while (isRunning) {
+            System.out.println("메뉴를 고르세요. ( c: 계산기, h: 히스토리, exit: 종료 )");
+            String menu = scanner.next();
+            switch (menu) {
+                case "c" -> {
+                    calculatorMenu(scanner);
+                    break;
+                }
+                case "h" -> {
+                    historyMenu(scanner);
+                    break;
+                }
+                case "exit" -> {
+                    System.out.println("계산기 프로그램을 종료합니다.");
+                    isRunning = false;
+                    break;
+                }
+                default -> System.out.println("잘못된 입력입니다. 다시 선택해주세요.");
+            }
+        }
+    }
+
+
+    private String getValidChoice(Scanner scanner) {
+        while (true) {
+            System.out.println("더 계산하시겠습니까? ( y: 연속, c: 클리어, h: 히스토리, q: 메뉴로 이동 )");
+            String choice = scanner.next();
+            if ("ychq".contains(choice)) return choice;
+            System.out.println("잘못된 입력입니다.");
+        }
+    }
+
+    private void calculatorMenu(final Scanner scanner) {
+        System.out.println("\n--- 계산기 ---");
+        boolean isRunning = true;
+        double lastResult = 0;
+        boolean hasLastResult = false;
+
+        while (isRunning) {
+            double num1 = hasLastResult ? lastResult : 0;
+            if (!hasLastResult) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                num1 = scanner.nextDouble();
+            }
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = scanner.next().charAt(0);
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            double num2 = scanner.nextDouble();
+
+            lastResult = calculate(num1, num2, operator);
+
+
+            System.out.println("결과: " + num1 + " " + operator + " " + num2 + " = " + lastResult);
+
+
+            System.out.println("더 계산하시겠습니까? ( y: 연속, c: 클리어, h: 히스토리, q: 메뉴로 이동 )");
+            String choice = scanner.next();
+
+            switch (choice) {
+                case "y": {
+                    hasLastResult = true;
+                    break;
+                }
+
+                case "c": {
+                    hasLastResult = false;
+                    lastResult = 0;
+                    System.out.println("값을 초기화했습니다.");
+                    break;
+                }
+
+                case "h": {
+                    hasLastResult = true;
+                    historyMenu(scanner);
+                    break;
+                }
+
+                case "q": {
+                    System.out.println("최종 결과값은: " + lastResult + " 입니다.");
+                    isRunning = false;
+                    break;
+                }
+                default: {
+                    System.out.println("잘못된 입력입니다. 다시 선택해주세요.");
+                    break;
+                }
+
+            }
+        }
+    }
+
+
+    public void removeOldestHistory() {
+        if (!history.isEmpty()) {
+            history.remove(0);
+        }
+    }
+
+    private void historyMenu(final Scanner scanner) {
+        System.out.println("\n--- 계산 기록 ---");
+        boolean isRunning = true;
+
+        // 이때 내부 history값을 이용해야할지? 외부에 전달하는 history 메서드중 어떤걸 이용할지 고민이됩니다.
+        // 단방향 데이터 흐름을 지키고 싶어서 고민을 했습니다.
+        List<String> currentHistory = getHistory();
+
+        while (isRunning) {
+            if (currentHistory.isEmpty()) {
+                System.out.println("기록이 없습니다.");
+                return;
+            } else {
+                for (String history : currentHistory) {
+                    System.out.print(history);
+                }
+            }
+
+            System.out.println("\n1. 오래된 기록 삭제, 2. 뒤로 가기");
+            String choice = scanner.next();
+
+            switch (choice) {
+                case "1":
+                    removeOldestHistory();
+                    System.out.println("삭제되었습니다.");
+                    break;
+                case "2":
+                    isRunning = false;
+                    break;
+                default:
+                    System.out.println("잘못된 입력입니다. 다시 선택해주세요.");
+                    break;
+            }
+        }
+
+    }
+
+
+    private double calculate(
+            final double num1,
+            final double num2,
+            final char operator
+    ) {
+
+        double result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if (num2 == 0) {
+                    throw new ArithmeticException("분모 0으로 나눌수 없습니다.");
+                } else {
+                    result = num1 / num2;
+                }
+
+                break;
+
+            case '%':
+                result = num1 % num2;
+                break;
+
+            case '^':
+                result = Math.pow(num1, num2);
+                break;
+
+            case 'q':
+                result = Math.sqrt(num1);
+                break;
+        }
+
+        // history
+        String record = num1 + " " + operator + " " + num2 + " = " + result;
+        history.add(record);
+
+        return result;
+    }
+}
--- src/com/sparta/Main.java
+++ src/com/sparta/Main.java
@@ -1,95 +1,13 @@
 package com.sparta;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        double lastResult = 0;
-        boolean hasLastResult = false;
-        System.out.println("===나는 계산기야===");
-        Scanner scanner = new Scanner(System.in);
+        // 계산기
+        Calculator calc = new Calculator();
+        calc.start();
 
-        while (true) {
-            double num1 = lastResult;
-            if (!hasLastResult) {
-                System.out.print("첫 번째 숫자를 입력하세요: ");
-                num1 = scanner.nextDouble();
-            }
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = scanner.next().charAt(0);
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            double num2 = scanner.nextDouble();
-
-            lastResult = calculate(num1, num2, operator);
-            System.out.println(num1 + " " + operator + " " + num2 + " = " + lastResult);
-            System.out.println("더 계산하시겠습니까? (y: 계산, c: 클리어, exit: 종료)");
-            String choice = scanner.next();
-
-            if (choice.equals("exit")) {
-                System.out.println("계산기가 종료됩니다. 최종 결과값은 " + lastResult + " 입니다.");
-                break;
-            } else if (choice.equals("y")) {
-                hasLastResult = true;
-            } else if (choice.equals("c")) {
-                hasLastResult = false;
-                lastResult = 0;
-                System.out.println("값을 초기화했습니다.");
-            }
-        }
     }
 
-    private static double calculate(
-            final double num1,
-            final double num2,
-            final char operator
-    ) {
 
-        double result = 0;
-        switch (operator) {
-            case '+': {
-                result = num1 + num2;
-                break;
-            }
-
-            case '-': {
-                result = num1 - num2;
-                break;
-            }
-
-            case '*': {
-                result = num1 * num2;
-                break;
-            }
-
-            case '/': {
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    result = num2;
-                    System.out.println("분모 0으로 나눌수 없습니다.");
-                }
-
-                break;
-            }
-
-            case '%': {
-                result = num1 % num2;
-                break;
-            }
-
-            case '^': {
-                result = Math.pow(num1, num2);
-                break;
-            }
-
-            case 'q': {
-                result = Math.sqrt(num1);
-                break;
-            }
-
-        }
-
-        return result;
-    }
 }
